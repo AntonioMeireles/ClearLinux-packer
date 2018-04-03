ef89b1d958424b08872461b4f6d672ec332df1f9
António Meireles <antonio.meireles@reformi.st>
2018-04-03 19:56:19 +0100

    increase default root volume size to 10Gb
    Signed-off-by: António Meireles <antonio.meireles@reformi.st>

--- scripts/01_ClearLinux_setup.sh
+++ scripts/01_ClearLinux_setup.sh
@@ -5,6 +5,12 @@ set -o pipefail
 set -o nounset
 set -o xtrace
 
-swupd bundle-add sysadmin-basic network-basic storage-utils
+swupd bundle-add sysadmin-basic storage-utils
 
+MAXSIZEMB=$(printf %s\\n 'unit MB print list' | parted | grep "Disk /dev/sda" | cut -d' ' -f3 | tr -d MB)
+echo -e "F\n3\nYes\n${MAXSIZEMB}MB\n" | parted /dev/sda ---pretend-input-tty resizepart
+partprobe /dev/sda
+resize2fs /dev/sda3
+
+swupd bundle-add network-basic
 timedatectl set-ntp true
--- scripts/02_vagrant_user.sh
+++ scripts/02_vagrant_user.sh
@@ -9,7 +9,7 @@ export VAGRANT_USER="${VAGRANT_USER:-clear}"
 export VAGRANT_PASSWORD='$6$rounds=656000$OY1EmeRe9//dqf8D$KRUcAe5ezDDL4hDe7nCGdURxev0jnIpOAAtfFzhPdd9wmNouedwX7EMxUaF16yrxxOUgpQlrpHVsZkIokXDKv0'
 export VAGRANT_HOME="/home/${VAGRANT_USER}"
 export DOT_SSH="${VAGRANT_HOME}/.ssh"
-export KEYS_URL="https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/"
+export KEYS_URL="https://raw.githubusercontent.com/mitchellh/vagrant/master/keys"
 export SUDOERS="/etc/sudoers.d/vagrant"
 
 useradd --create-home --user-group --password ${VAGRANT_PASSWORD} "${VAGRANT_USER}"
@@ -27,8 +27,8 @@ chmod 0440 "${SUDOERS}"
 
 mkdir -p "${DOT_SSH}"
 
-curl -o "${DOT_SSH}/vagrant" "${KEYS_URL}/vagrant"
-curl -o "${DOT_SSH}/vagrant.pub" "${KEYS_URL}/vagrant.pub"
+curl -o "${DOT_SSH}/vagrant" -sSL "${KEYS_URL}/vagrant"
+curl -o "${DOT_SSH}/vagrant.pub" -sSL "${KEYS_URL}/vagrant.pub"
 
 cat "${DOT_SSH}/vagrant.pub" >> "${DOT_SSH}/authorized_keys"
 
