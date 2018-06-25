fb3d2f7653d42c3ce0039827f5d10f14256616ad
António Meireles <antonio.meireles@reformi.st>
2018-06-25 11:12:44 +0100

    add 'editors' and 'shells' bundles to default install (per #5)
    Signed-off-by: António Meireles <antonio.meireles@reformi.st>

--- scripts/01_ClearLinux_setup.sh
+++ scripts/01_ClearLinux_setup.sh
@@ -12,8 +12,8 @@ echo -e "F\n3\nYes\n${MAXSIZEMB}MB\n" | parted /dev/sda ---pretend-input-tty res
 partprobe /dev/sda
 resize2fs /dev/sda3
 
-swupd bundle-add network-basic
-timedatectl set-ntp true
+swupd bundle-add network-basic editors shells
+# timedatectl set-ntp true
 
 systemctl mask tallow
 
