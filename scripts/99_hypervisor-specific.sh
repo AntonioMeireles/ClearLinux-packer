b5e77c98202f3e25bace5d43bfd21165eef5a86d
António Meireles <antonio.meireles@reformi.st>
2018-07-30 10:31:03 +0100

    refactor hypervisor specific costumizations
      instead of being done in the 'boot_command' step they are now the last
      thing to be called for.
      the reason is that we need to do them - specifically in the virtualbox
      case - well after all invocations to 'swupd' are finished, as it calls
      'clr-boot-manager update' internally. otherwise one would get an extra
      10s delay on boot if/when we swap the (default) kernel...
    
    Signed-off-by: António Meireles <antonio.meireles@reformi.st>

---
+++ scripts/99_hypervisor-specific.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace
+
+export HYPERVISOR="$(systemd-detect-virt -v)"
+
+case "${HYPERVISOR}" in
+	'vmware')
+		echo "VMware detected..."
+		swupd bundle-add os-cloudguest-vmware
+		systemctl enable open-vm-tools
+	;;
+	'oracle')
+	echo "VirtualBox detected..."
+		swupd bundle-add kernel-lts
+		swupd bundle-remove kernel-native
+		mount /dev/sda1 /boot
+		echo 'default '$(ls /boot/loader/entries/ | grep lts | sed -e 's/.conf$//') > /boot/loader/loader.conf
+		rm -rfv /boot/*/*/*native*
+		umount /boot
+	;;
+esac
