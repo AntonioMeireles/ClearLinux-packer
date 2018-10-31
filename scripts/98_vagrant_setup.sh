ad3e46ced5eea1dad2bf14b1027db63b086049b7
António Meireles <antonio.meireles@reformi.st>
2018-10-31 18:45:21 +0000

    drop not needed anymore sshd workaround
    Signed-off-by: António Meireles <antonio.meireles@reformi.st>

--- scripts/98_vagrant_setup.sh
+++ scripts/98_vagrant_setup.sh
@@ -45,7 +45,6 @@ chmod 0600 "${DOT_SSH}/vagrant"
 	echo 'PasswordAuthentication no'
 	echo 'PermitRootLogin no'
 	echo 'AuthorizedKeysFile %h/.ssh/authorized_keys'
-	echo 'Ciphers +aes128-cbc'
 } > /etc/ssh/sshd_config
 chmod 0600 /etc/ssh/sshd_config
 
