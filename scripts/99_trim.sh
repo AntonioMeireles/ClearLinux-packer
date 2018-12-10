2afc8a4a7de75ddf3c9c81f45756264acb57783c
António Meireles <antonio.meireles@reformi.st>
2018-12-10 13:03:58 +0000

    enable trim support and QCOW2 compression on libvirt boxes
    Signed-off-by: António Meireles <antonio.meireles@reformi.st>

--- scripts/99_trim.sh
+++ scripts/99_trim.sh
@@ -5,17 +5,19 @@ set -o pipefail
 set -o nounset
 set -o xtrace
 
-if [[ "${PACKER_BUILDER_TYPE}" != "qemu" ]]; then
-    echo '- Zeroing out empty space for better compressability'
+echo '- Zeroing out empty space for better compressability'
+systemctl start boot.mount
+if [[ "${PACKER_BUILDER_TYPE}" == "qemu" ]]; then
+    fstrim -av
+    systemctl enable fstrim.timer
+else
     # /
     dd if=/dev/zero of=/EMPTY bs=1M || echo "dd exit code $? is suppressed"
     rm -f /EMPTY
 
-    systemctl start boot.mount
     # /boot
     dd if=/dev/zero of=/boot/EMPTY bs=1M || echo "dd exit code $? is suppressed"
     rm -f /boot/EMPTY
-    systemctl stop boot.mount
 
     set +e
     swapuuid="$(/sbin/blkid -o value -l -s UUID -t TYPE=swap)"
@@ -27,6 +29,9 @@ if [[ "${PACKER_BUILDER_TYPE}" != "qemu" ]]; then
         dd if=/dev/zero of="${swappart}" bs=1M || echo "dd exit code $? is suppressed"
         /sbin/mkswap -U "${swapuuid}" "${swappart}"
     fi
-    sync
 fi
+systemctl stop boot.mount
+sync
+
+
 
