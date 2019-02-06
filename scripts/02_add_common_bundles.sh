8696151d3bc18f9912bbcdaa5807ddf73b123157
António Meireles <antonio.meireles@reformi.st>
2019-02-06 17:12:57 +0000

    fulfill #12 intent for real this time
    Signed-off-by: António Meireles <antonio.meireles@reformi.st>

--- scripts/02_add_common_bundles.sh
+++ scripts/02_add_common_bundles.sh
@@ -8,5 +8,6 @@ set -o xtrace
 systemctl start boot.mount
 
 swupd bundle-add sysadmin-basic network-basic vim shells containers-basic patch diffutils
-# We are not enabling docker by default as it is not required for all
-# use-cases. Kindly enable the docker service if you wish to use it.
+# We are not activating docker by default as it is not required for all
+# use-cases. `sudo systemctl unmask docker.service` if you wish to use it.
+systemctl mask docker
