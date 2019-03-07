303a77624788a4ab0530dfa587fcfbc104efd9b7
António Meireles <antonio.meireles@reformi.st>
2019-03-07 16:45:56 +0000

    raise minimum guest plugin rev to 1.1.1
    Signed-off-by: António Meireles <antonio.meireles@reformi.st>

--- Vagrantfile.template.rb
+++ Vagrantfile.template.rb
@@ -9,7 +9,7 @@ unless ['plugin'].include? ARGV[0]
   [
     {
       name: 'vagrant-guests-clearlinux',
-      version: '>= 1.1.0'
+      version: '>= 1.1.1'
     }
   ].each do |plugin|
     next if Vagrant.has_plugin?(plugin[:name], plugin[:version])
