41cc7dcdea770a6bce9927b117b299a4a78f827b
António Meireles <antonio.meireles@reformi.st>
2019-04-29 13:08:21 +0100

    refactor to ease vagrant use over ClearLinux
    - en passant the bundled native libvirt VM now installs Vagrant inside
      by default
    
    Signed-off-by: António Meireles <antonio.meireles@reformi.st>

---
+++ extras/clearlinux/setup/libvirtd.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace
+
+[[ -x "$(command -v virsh)" ]] || {
+  echo "Aborted: you do not appear to have the 'kvm-host' bundle installed"
+  echo "         please run 'sudo swupd kvm-host' and then try again"
+  exit 1
+}
+sudo systemctl enable libvirtd --now
+
+for group in kvm libvirt; do
+  sudo usermod -G ${group} -a clear
+done
+
+sudo mkdir -p /var/lib/libvirt/{isos,images}/ /etc/profile.d/ /usr/local/bin
+sudo chown root:kvm /var/lib/libvirt/{isos,images}/
+sudo chmod g+rwx /var/lib/libvirt/{isos,images}/
+
+echo 'export LIBVIRT_DEFAULT_URI="qemu:///system"' | sudo tee /etc/profile.d/libvirt.conf
---
+++ extras/clearlinux/setup/packer.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace
+
+PACKER_VERSION=1.4.0
+PACKER_ZIP=packer_${PACKER_VERSION}_linux_amd64.zip
+PACKER_URL=https://releases.hashicorp.com/packer/${PACKER_VERSION}/${PACKER_ZIP}
+
+curl -s ${PACKER_URL} -o ${PACKER_ZIP}
+unzip -qq ${PACKER_ZIP} && sudo mv packer /usr/local/bin && rm -rf ${PACKER_ZIP}
