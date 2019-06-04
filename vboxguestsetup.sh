82405c07a79921deed5944de7dc704534151e649
António Meireles <antonio.meireles@reformi.st>
2019-06-04 12:55:36 +0100

    refactor vbox guest tools provisioning setup script
    Signed-off-by: António Meireles <antonio.meireles@reformi.st>

--- vboxguestsetup.sh
+++ vboxguestsetup.sh
@@ -1,54 +1,79 @@
 #!/usr/bin/env bash
 
-mountpoint=$(mktemp -d -t tmp.XXXXXXXXXX)
-unpack=$(mktemp -d -t tmp.XXXXXXXXXX)
-store=$(mktemp -d -t tmp.XXXXXXXXXX)
+default="/home/clear/VBoxGuestAdditions.iso"
 
-function cleanup {
+mountpoint=$(mktemp -d -t vbga.XXXXXXXXXX)
+guest_additions=$(mktemp -d -t vbga.XXXXXXXXXX)
+linux_bits=$(mktemp -d -t vbga.XXXXXXXXXX)
+
+function error() {
+  echo "E: $*" >>/dev/stderr
+  exit 1
+}
+
+function cleanup() {
   umount "${mountpoint}"
-  rm -rf "${unpack}" /home/clear/VBoxGuestAdditions.iso
-  rm -rf "${mountpoint}" "${store}"
+  rm -rf "${guest_additions}" "${default}" "${mountpoint}" "${linux_bits}"
 }
 
 trap cleanup EXIT
 
-mount /home/clear/VBoxGuestAdditions.iso "${mountpoint}" -t iso9660 -o ro
-"${mountpoint}"/VBoxLinuxAdditions.run --noexec --keep --nox11 --target "${unpack}"
+if [ "$(id -u)" -ne 0 ]; then
+  error "You must be 'root' to execute this script"
+fi
 
-tar xjf "${unpack}"/VBoxGuestAdditions-amd64.tar.bz2 -C "${store}"
+if [[ -f "${default}" ]]; then
+  mount "${default}" "${mountpoint}" -t iso9660 -o ro
+else
+  drives_found=$(lsblk | grep sr | cut -c -3)
+  if [ -z "${drives_found}" ]; then
+    error 'CDROM drives NOT FOUND - Please attach the Guest Additions CD image...'
+  fi
+  ga_found=0
+  for cdrom in ${drives_found}; do
+    mount "/dev/${cdrom}" "${mountpoint}" -t iso9660 -o ro
+    if [ -f "${mountpoint}/VBoxLinuxAdditions.run" ]; then
+      ga_found=1
+      break
+    fi
+    umount "${mountpoint}"
+  done
+  [[ ga_found -eq 1 ]] || error 'CDROM drives NOT FOUND - Please attach the Guest Additions CD image...'
+fi
 
-install -m0755 "${store}"/bin/{VBoxClient,VBoxControl} /usr/bin/
-install -m0755 "${store}"/sbin/VBoxService /usr/bin/
-install -m0755 "${store}"/other/mount.vboxsf /usr/bin/
-install -m0755 -D "${store}"/other/98vboxadd-xclient /usr/bin/VBoxClient-all
-install -Dm0644 "${store}"/other/vboxclient.desktop /usr/share/xdg/autostart/vboxclient.desktop
+"${mountpoint}/VBoxLinuxAdditions.run" --noexec --keep --nox11 --target "${guest_additions}"
+tar xjf "${guest_additions}/VBoxGuestAdditions-amd64.tar.bz2" -C "${linux_bits}"
 
-install -m0755 "${store}"/lib/VBoxOGL*.so /usr/lib64/
-install -d /usr/lib64/dri
-ln -s /usr/lib64/VBoxOGL.so /usr/lib64/dri/vboxvideo_dri.so
+for f in "${linux_bits}"{/bin/{VBoxClient,VBoxControl},/sbin/VBoxService,/other/mount.vboxsf}; do
+  install -m0755 "${f}" '/usr/bin'
+done
 
-install -Dm0755 -D "${store}"/other/pam_vbox.so /usr/lib64/security/pam_vbox.so
+install -Dm0755 "${linux_bits}/other/98vboxadd-xclient" '/usr/bin/VBoxClient-all'
+install -Dm0644 "${linux_bits}/other/vboxclient.desktop" '/usr/share/xdg/autostart/vboxclient.desktop'
 
-install -d /etc/udev/rules.d
-(
-  echo 'ACTION=="add", KERNEL=="vboxguest", SUBSYSTEM=="misc", OWNER="root", MODE="0600"'
-  echo 'ACTION=="add", KERNEL=="vboxuser", SUBSYSTEM=="misc", OWNER="root", MODE="0666"'
-) | tee /etc/udev/rules.d/60-vboxguest.rules
-chmod 0644 /etc/udev/rules.d/60-vboxguest.rules
+install -m0755 "${linux_bits}/lib/"VBoxOGL*.so '/usr/lib64/'
+install -d '/usr/lib64/dri'
+ln -s '/usr/lib64/VBoxOGL.so' '/usr/lib64/dri/vboxvideo_dri.so'
 
-(
-  echo '[Unit]'
-  echo 'Description=VirtualBox Guest Service'
-  echo 'ConditionVirtualization=oracle'
-  echo '[Service]'
-  echo 'ExecStartPre=-/usr/bin/modprobe vboxguest'
-  echo 'ExecStartPre=-/usr/bin/modprobe vboxvideo'
-  echo 'ExecStartPre=-/usr/bin/modprobe vboxsf'
-  echo 'ExecStart=/usr/bin/VBoxService -f'
-  echo '[Install]'
-  echo 'WantedBy=multi-user.target'
-) | tee /etc/systemd/system/vboxservice.service
-chmod 0644 /etc/systemd/system/vboxservice.service
-install -d /etc/sysusers.d
-echo "g vboxsf 109 -" | tee /etc/sysusers.d/virtualbox-guest-utils.conf
-chmod 0644 /etc/sysusers.d/virtualbox-guest-utils.conf
+install -Dm0755 -D "${linux_bits}/other/pam_vbox.so" '/usr/lib64/security/pam_vbox.so'
+
+install -d '/etc/udev/rules.d'
+printf %s '
+ACTION=="add", KERNEL=="vboxguest", SUBSYSTEM=="misc", OWNER="root", MODE="0600"
+ACTION=="add", KERNEL=="vboxuser", SUBSYSTEM=="misc", OWNER="root", MODE="0666"
+' | tee '/etc/udev/rules.d/60-vboxguest.rules'
+chmod 0644 '/etc/udev/rules.d/60-vboxguest.rules'
+
+printf %s '
+[Unit]
+Description=VirtualBox Guest Service
+ConditionVirtualization=oracle
+[Service]
+ExecStartPre=-/usr/bin/modprobe vboxguest
+ExecStartPre=-/usr/bin/modprobe vboxvideo
+ExecStartPre=-/usr/bin/modprobe vboxsf
+ExecStart=/usr/bin/VBoxService -f
+[Install]
+WantedBy=multi-user.target
+' | tee '/etc/systemd/system/vboxservice.service'
+chmod 0644 '/etc/systemd/system/vboxservice.service'
