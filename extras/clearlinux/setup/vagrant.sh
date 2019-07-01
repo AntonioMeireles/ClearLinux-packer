9026ae692ec4315ef23038dc544c13b8ce96278d
António Meireles <antonio.meireles@reformi.st>
2019-07-01 15:20:01 +0100

    clean up a bit vagrant installer
    Signed-off-by: António Meireles <antonio.meireles@reformi.st>

--- extras/clearlinux/setup/vagrant.sh
+++ extras/clearlinux/setup/vagrant.sh
@@ -36,21 +36,16 @@ function cleanup() {
 trap cleanup EXIT
 
 pushd ${source_dir}
-  git clone https://github.com/hashicorp/vagrant-installers || true
+  git clone https://github.com/hashicorp/vagrant-installers
   substrate_dir="${source_dir}/vagrant-installers/substrate"
-  gem build vagrant.gemspec
 
   pushd "${substrate_dir}/launcher"
     go get github.com/mitchellh/osext
     go build -o vagrant
   popd
 
-  sudo mkdir -p "${EMBEDDED_DIR}/etc/"
-  echo "gem: --no-document --no-user-install" | sudo tee "${EMBEDDED_DIR}/etc/gemrc"
-  sudo chmod 0644 "${EMBEDDED_DIR}/etc/gemrc"
-
-  sudo mkdir -p ${EMBEDDED_DIR}/rgloader/
-  sudo install -m644 ${substrate_dir}/{linux,common}/rgloader/* "${EMBEDDED_DIR}/rgloader/"
+  sudo install -D -m 0644 ${substrate_dir}/common/gemrc "${EMBEDDED_DIR}/etc/gemrc"
+  sudo install -D -m 0644 ${substrate_dir}/{linux,common}/rgloader/* -t "${EMBEDDED_DIR}/rgloader/"
 
   export GEM_PATH="${EMBEDDED_DIR}/gems/${VAGRANT_VERSION}"
   export GEM_HOME="${GEM_PATH}"
@@ -58,9 +53,12 @@ pushd ${source_dir}
 
   sudo mkdir -p ${GEM_PATH}
 
-  sudo -E gem install pkg-config vagrant-${VAGRANT_VERSION}.gem
+  gem build vagrant.gemspec
+  sudo -E gem install pkg-config vagrant-${VAGRANT_VERSION}.gem --no-document
 
-  sudo install -Dm755 "${substrate_dir}/launcher/vagrant" /opt/vagrant/bin/vagrant
+  sudo rm -rf ${GEM_PATH}/gems/vagrant-${VAGRANT_VERSION}/vagrant-installers
+
+  sudo install -D -m 0755 "${substrate_dir}/launcher/vagrant" /opt/vagrant/bin/vagrant
   sudo ln -sf /opt/vagrant/bin/vagrant /usr/local/bin/
 
   echo '{"vagrant_version": "'${VAGRANT_VERSION}'"}' | sudo tee ${EMBEDDED_DIR}/manifest.json
