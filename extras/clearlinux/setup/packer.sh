3cbeaf173d1492deb7f7e139df81e14252b7c86d
António Meireles <antonio.meireles@reformi.st>
2020-02-17 10:50:56 +0000

    bump suggested packer version (to 1.5.4)
    en passant we no longer rely on 'go get' to get blob built, instead we
    now strictly rely on the upstream build machinery
    
    Signed-off-by: António Meireles <antonio.meireles@reformi.st>

--- extras/clearlinux/setup/packer.sh
+++ extras/clearlinux/setup/packer.sh
@@ -5,20 +5,32 @@ set -o pipefail
 set -o nounset
 set -o xtrace
 
-export PACKER_VERSION=1.4.5
+PACKER_VERSION=1.5.4
 
 GOPATH="$(mktemp -d)"
+BUILDPATH="$(mktemp -d)"
+GO111MODULE=auto
+BUILDPATCH="$(dirname "${BASH_SOURCE[0]}")/justNativePackerBuild.patch"
+
+export PACKER_VERSION
 export GOPATH
-export GO111MODULE=on
+export GO111MODULE
+export BUILDPATH
+export BUILDPATCH
 
 function cleanup() {
   sudo rm -rf "${GOPATH}"
+  sudo rm -rf "${BUILDPATH}"
 }
 
 trap cleanup EXIT
 
-sudo swupd bundle-add {c,ruby,go}-basic
+sudo swupd bundle-add {c,ruby,go}-basic git
 
-go get github.com/hashicorp/packer@v${PACKER_VERSION}
+git clone https://github.com/hashicorp/packer "${BUILDPATH}"
 
+pushd "${BUILDPATH}"
+git checkout "v${PACKER_VERSION}"
+patch -p1 < "${BUILDPATCH}"
+make releasebin
 sudo install -Dm0755 "${GOPATH}/bin/packer" /usr/local/bin
