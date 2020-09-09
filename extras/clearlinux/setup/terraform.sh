76be264f3cfbc4c7a264ce690adf7ce671f3557f
António Meireles <antonio.meireles@reformi.st>
2020-09-09 16:49:01 +0100

    bump terraform to 0.13.2 and matching tf libvirt provider to 0.6.2
    Signed-off-by: António Meireles <antonio.meireles@reformi.st>

--- extras/clearlinux/setup/terraform.sh
+++ extras/clearlinux/setup/terraform.sh
@@ -4,23 +4,40 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-export TERRAFORM_PLUGIN_DIR=~/.terraform.d/plugins
-export TERRAFORM_VERSION=0.12.4
-export TERRAFORM_PROVIDER_LIBVIRT_VERSION=0.5.2
+
+export TERRAFORM_VERSION=0.13.2
+export TERRAFORM_PROVIDER_LIBVIRT_VERSION=0.6.2
+export LIBVIRT_PLUGIN_DIR=~/.local/share/terraform/plugins/registry.terraform.io/dmacvicar/libvirt/${TERRAFORM_PROVIDER_LIBVIRT_VERSION}/linux_amd64
+
+export TERRAFORM_URL=https://github.com/hashicorp/terraform/archive/v${TERRAFORM_VERSION}.zip
+
 GOPATH="$(mktemp -d)"
 export GOPATH
 export GO111MODULE=on
 
 function cleanup() {
-  sudo rm -rf "${GOPATH}"
+  sudo rm -rf "${source_dir}" "${GOPATH}" "${zipfile}"
 }
 
 trap cleanup EXIT
 
 sudo swupd bundle-add {c,go}-basic xorriso
 
-go get github.com/hashicorp/terraform@v${TERRAFORM_VERSION}
-sudo install -Dvm0755 "${GOPATH}/bin/terraform" -t /usr/local/bin/
+zipfile="$(mktemp)"
+curl -sL ${TERRAFORM_URL} -o "${zipfile}"
+unzip -qq "${zipfile}" -d /tmp
 
-go get github.com/dmacvicar/terraform-provider-libvirt@v${TERRAFORM_PROVIDER_LIBVIRT_VERSION}
-install -Dvm0755 "${GOPATH}/bin/terraform-provider-libvirt" ${TERRAFORM_PLUGIN_DIR}/terraform-provider-libvirt
+source_dir="/tmp/terraform-${TERRAFORM_VERSION}"
+
+pushd ${source_dir}
+  export PATH="${GOPATH}/bin:${PATH}"
+  export XC_OS="linux"
+  export XC_ARCH="amd64"
+  ./scripts/build.sh
+  sudo install -Dvm0755 "${GOPATH}/bin/terraform" -t /usr/local/bin/
+
+  go get github.com/dmacvicar/terraform-provider-libvirt@v${TERRAFORM_PROVIDER_LIBVIRT_VERSION}
+  install -Dvm0755 "${GOPATH}/bin/terraform-provider-libvirt" ${LIBVIRT_PLUGIN_DIR}/terraform-provider-libvirt
+popd
+
+
