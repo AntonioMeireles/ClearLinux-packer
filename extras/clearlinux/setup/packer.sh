d27139333bc8c002039d9ebf3b1bd012e43ed378
António Meireles <antonio.meireles@reformi.st>
2021-01-21 12:29:27 +0000

    ensure that packer build script is idempotent
    was failing if run from a Vagrant provisioner
    
    Signed-off-by: António Meireles <antonio.meireles@reformi.st>

--- extras/clearlinux/setup/packer.sh
+++ extras/clearlinux/setup/packer.sh
@@ -10,13 +10,11 @@ PACKER_VERSION=1.6.6
 GOPATH="$(mktemp -d)"
 BUILDPATH="$(mktemp -d)"
 GO111MODULE=auto
-BUILDPATCH="$(cd `dirname $0` && pwd)/justNativePackerBuild.patch"
 
 export PACKER_VERSION
 export GOPATH
 export GO111MODULE
 export BUILDPATH
-export BUILDPATCH
 
 function cleanup() {
   sudo rm -rf "${GOPATH}"
@@ -31,6 +29,9 @@ git clone https://github.com/hashicorp/packer "${BUILDPATH}"
 
 pushd "${BUILDPATH}"
 git checkout "v${PACKER_VERSION}"
-patch -p1 < "${BUILDPATCH}"
+
+sed -i 's/^ALL_XC_ARCH=.*/ALL_XC_ARCH="amd64"/' scripts/build.sh
+sed -i 's/^ALL_XC_OS=.*/ALL_XC_OS="linux"/' scripts/build.sh
+
 make releasebin
 sudo install -Dm0755 "${GOPATH}/bin/packer" /usr/local/bin
