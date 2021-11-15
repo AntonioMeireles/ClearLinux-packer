35444f5ddf6428cdb05045b97a5c2103959b1075
António Meireles <antonio.meireles@reformi.st>
2021-11-15 14:01:33 +0000

    bump packer to 1.7.8
    - hacky until CL's go stack get up-to-date
    
    Signed-off-by: António Meireles <antonio.meireles@reformi.st>

--- extras/clearlinux/setup/packer.sh
+++ extras/clearlinux/setup/packer.sh
@@ -5,7 +5,7 @@ set -o pipefail
 set -o nounset
 set -o xtrace
 
-PACKER_VERSION=1.7.6
+PACKER_VERSION=1.7.8
 
 GOPATH="$(mktemp -d)"
 BUILDPATH="$(mktemp -d)"
@@ -32,6 +32,14 @@ git checkout "v${PACKER_VERSION}"
 
 sed -i 's/^ALL_XC_ARCH=.*/ALL_XC_ARCH="amd64"/' scripts/build.sh
 sed -i 's/^ALL_XC_OS=.*/ALL_XC_OS="linux"/' scripts/build.sh
+
+# ... until ClearLinux ships go-1.17 or higher ...
+go mod download github.com/aws/aws-sdk-go
+go get github.com/hashicorp/hcl/v2@v2.10.1
+go get github.com/hashicorp/packer-plugin-sdk/multistep/commonsteps@v0.2.7
+go get github.com/hashicorp/packer-plugin-sdk/rpc@v0.2.7
+go get github.com/prometheus/client_golang/prometheus@v1.11.0
+#
 
 make releasebin
 sudo install -Dm0755 "${GOPATH}/bin/packer" /usr/local/bin
